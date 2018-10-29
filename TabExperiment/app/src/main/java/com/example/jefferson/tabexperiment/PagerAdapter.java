2d0b6b2c86fca3d5548dd8b88b970182405c06f0
jjsislema <jeffersonsislema@gmail.com>
2018-10-29 03:12:09 -0400

    tab experiment

---
+++ TabExperiment/app/src/main/java/com/example/jefferson/tabexperiment/PagerAdapter.java
@@ -0,0 +1,34 @@
+package com.example.jefferson.tabexperiment;
+
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+
+public class PagerAdapter extends FragmentStatePagerAdapter {
+    int mNumOfTabs;
+    public PagerAdapter(FragmentManager fm, int NumOfTabs) {
+        super(fm);
+        this.mNumOfTabs = NumOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new TabFragment1();
+            case 1:
+                return new TapFragment2();
+            case 2:
+                return new TapFragment3();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return mNumOfTabs;
+    }
+}
